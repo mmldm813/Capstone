f96bec6a6fdf12bc7cfde71d0ac7cc8afc524262
michellemedina <michelle200med@gmail.com>
2019-01-24 13:34:35 -0500

    Database changes:
    - database no longer running on MainThread
    - AppExecutor class added

--- app/src/main/java/com/example/android/capstone/database/AppDatabase.java
+++ app/src/main/java/com/example/android/capstone/database/AppDatabase.java
@@ -29,8 +29,6 @@ public abstract class AppDatabase extends RoomDatabase {
                 Log.d(LOG_TAG, "Creating new database instance");
                 instance = Room.databaseBuilder(context.getApplicationContext(),
                         AppDatabase.class, AppDatabase.DATABASE_NAME)
-                        //change this back to a separate thread
-                        .allowMainThreadQueries()
                         .build();
             }
         }
